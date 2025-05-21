d6aaed93992833fdecd45d2e16c860b3fec8f989
zero1.ai <145408852+zero1ai@users.noreply.github.com>
2025-05-20 22:59:48 -0700

    update __init__.py
    optional symbolic-self addon

--- fracton/__init__.py
+++ fracton/__init__.py
@@ -1,2 +1,9 @@
 __version__ = "0.2.0"
 from .core import emergent_field, P
+
+# --- symbolic-self addon -----------------------------
+try:
+    from .symbolic.model import SymbolicSelf  # re-export for users
+except ModuleNotFoundError:
+    # running on an install that doesn’t include the new module yet
+    pass
